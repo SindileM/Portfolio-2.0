cedf8d4eadf1771a0dcb4136ec025bdcd7492020
Sindile <smakhathini24@gmail.com>
2021-11-25 22:57:46 +0200

    updates

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
   });
+
+  var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+    direction: 'vertical',
+    loop: false,
+    speed: 1600,
+    pagination: '.swiper-pagination',
+    paginationBulletRender: function (swiper, index, className) {
+      var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+      return '<span class="' + className + '">' + year + '</span>';
+    },
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    breakpoints: {
+      768: {
+        direction: 'horizontal',
+      }
+    }
+  });
