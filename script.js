2af878ec3224c9b5c36ddb0270185b98194d45d0
SindileM <smakhathini24@gmail.com>
2021-11-26 16:01:03 +0200

    changes and updates

--- script.js
+++ script.js
@@ -7,6 +7,40 @@ $(document).ready(function () {
     }, 100*i);
   });
 });
+// Work Experience
+// Educational Background
+(function () {
+  "use strict";
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();
 
 // Contact Form
 document.querySelector('#contact-form').addEventListener('submit', (e) => {
@@ -16,21 +50,3 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.message.value = '';
   });
 
-  // var timelineSwiper = new Swiper ('.timeline .swiper-container', {
-  //   direction: 'vertical',
-  //   loop: false,
-  //   speed: 1600,
-  //   pagination: '.swiper-pagination',
-  //   paginationBulletRender: function (swiper, index, className) {
-  //     var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
-  //     return '<span class="' + className + '">' + year + '</span>';
-  //   },
-  //   paginationClickable: true,
-  //   nextButton: '.swiper-button-next',
-  //   prevButton: '.swiper-button-prev',
-  //   breakpoints: {
-  //     768: {
-  //       direction: 'horizontal',
-  //     }
-  //   }
-  // });
