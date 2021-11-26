d0cdbd9b739e3f045a9c99fd7db8aa8404babbcc
Sindile <smakhathini24@gmail.com>
2021-11-26 05:36:16 +0200

    changes

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Skills Section
+$(document).ready(function () {
+  $('.skill-icons').children('.active').each(function(i) {
+    var row = $(this);
+    setTimeout(function() {
+      row.css('background','#c0392b');
+    }, 100*i);
+  });
+});
+
+// Contact Form
 document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.preventDefault();
     e.target.elements.name.value = '';
@@ -5,21 +16,21 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.message.value = '';
   });
 
-  var timelineSwiper = new Swiper ('.timeline .swiper-container', {
-    direction: 'vertical',
-    loop: false,
-    speed: 1600,
-    pagination: '.swiper-pagination',
-    paginationBulletRender: function (swiper, index, className) {
-      var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
-      return '<span class="' + className + '">' + year + '</span>';
-    },
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    breakpoints: {
-      768: {
-        direction: 'horizontal',
-      }
-    }
-  });
+  // var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+  //   direction: 'vertical',
+  //   loop: false,
+  //   speed: 1600,
+  //   pagination: '.swiper-pagination',
+  //   paginationBulletRender: function (swiper, index, className) {
+  //     var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+  //     return '<span class="' + className + '">' + year + '</span>';
+  //   },
+  //   paginationClickable: true,
+  //   nextButton: '.swiper-button-next',
+  //   prevButton: '.swiper-button-prev',
+  //   breakpoints: {
+  //     768: {
+  //       direction: 'horizontal',
+  //     }
+  //   }
+  // });
